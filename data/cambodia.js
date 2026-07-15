3a7aac9e2e5e8ede6b5a0900abd58198d6bdc2c1
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-15 09:06:04 +0700

    Modify 'tunggal' and 'angka' values in cambodia.js
    Updated the 'tunggal' values and added new 'angka' entries for each day.

--- data/cambodia.js
+++ data/cambodia.js
@@ -3,49 +3,76 @@ senin: {
 angka: `
 
 `,
-tunggal: "8"
+tunggal: ""
 },
 
 selasa: {
 angka: `
-
+2065 : 37
+1207 : 64
+6126 : 85
+6781 : 02
+0925 : 68
+2665 : 02
 `,
-tunggal: "5"
+tunggal: "0"
 },
 
 rabu: {
 angka: `
-
+2065 : 37
+1207 : 64
+6126 : 85
+6781 : 02
+0925 : 68
+2665 : 02
+5202 : 18
 `,
-tunggal: "0"
+tunggal: "8"
 },
 
 kamis: {
 angka: `
 
 `,
-tunggal: "0"
+tunggal: ""
 },
 
 jumat: {
 angka: `
 
 `,
-tunggal: "0"
+tunggal: ""
 },
 
 sabtu: {
 angka: `
 
 `,
-tunggal: "0"
+tunggal: ""
 },
 
 minggu: {
 angka: `
 
 `,
-tunggal: "0"
+tunggal: ""
 
 }
 };
+        //Rumus sama ekor mmundur, jgn langsung
+        //0902 : 
+//6358 : 
+//4753 : 
+//5674 : 
+//6468 : 
+//7356 : 
+//6290 : pangkas yg sudah result 4d
+//2528 : 6475 647 bawah kanan
+//2065 : 3786 378 37
+//1207 : 8647 864 64 tunggal dluan ai 4digit
+//6126 : 0285 085 85
+//6781 : 5602 502 02
+//0925 : 6028 608 68
+//2665 : 6028 028 02 nice
+//5202 : 9018 918 18
