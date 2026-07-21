dfa052132f78e9d1d9f4a6559d03d06c949e52c8
ANGKA STATISTIK <angkastatistik@gmail.com>
2026-07-21 23:33:16 +0700

    Update angka_p6 and tunggal_p6 values

--- data/macau.js
+++ data/macau.js
@@ -16,7 +16,7 @@ const DATA_MACAU = {
         angka_p3: `8159 : 782`, tunggal_p3: "2",
         angka_p4: `3493 : 127`, tunggal_p4: "2",
         angka_p5: `4948 : 178`, tunggal_p5: "7",
-        angka_p6: ``, tunggal_p6: ""
+        angka_p6: `6785 : 012`, tunggal_p6: "0"
     },
 
     rabu: {
